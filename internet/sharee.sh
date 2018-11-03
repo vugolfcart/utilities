28dee008645472802e2901cbe78a571c7b942a99
Patrick Pei <14530807+patrickpei@users.noreply.github.com>
2018-11-03 16:39:40 -0500

    add sharee

---
+++ internet/sharee.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+
+route del gw 192.168.1.1
+route del default gw 192.168.1.1
+
+echo "nameserver 8.8.8.8" | sudo tee /etc/resolv.conf
+
+route add default gw 192.168.1.102 # use your own ip 
+
