762906f7d5b1f4ec890683dd01c1ec0eb648345c
Patrick Pei <14530807+patrickpei@users.noreply.github.com>
2018-12-01 03:53:43 -0600

    cleanup

--- internet/sharee.sh
+++ internet/sharee.sh
@@ -5,5 +5,4 @@ route del default gw 192.168.1.1
 
 echo "nameserver 8.8.8.8" | sudo tee /etc/resolv.conf
 
-route add default gw 192.168.1.102 # use your own ip 
-
+route add default gw 192.168.1.102 # use your own ip
