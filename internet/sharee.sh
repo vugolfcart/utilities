a4a3adc8873dbfb56d50b2965e9b677c5fcea947
Patrick Pei <14530807+patrickpei@users.noreply.github.com>
2019-01-26 17:19:37 -0600

    add IMU to launch

--- internet/sharee.sh
+++ internet/sharee.sh
@@ -5,5 +5,5 @@ route del default gw 192.168.1.1
 
 echo "nameserver 8.8.8.8" | sudo tee /etc/resolv.conf
 
-route add default gw 192.168.1.103 # use your own ip
+route add default gw 192.168.1.102 # use your own ip
 
